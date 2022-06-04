613f110248617fb25458ef9eabbae3583e2b29db
Les Matheson <les.matheson@gmail.com>
2022-06-04 11:22:43 -0400

    Renamed to tutorialmod-1

--- src/main/java/com/lmatheson/minecraft/tutorialmod1/TutorialMod1.java
+++ src/main/java/com/lmatheson/minecraft/tutorialmod1/TutorialMod1.java
@@ -1,4 +1,4 @@
-package com.lmatheson.minecraft.tutorialmod;
+package com.lmatheson.minecraft.tutorialmod1;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,14 +18,14 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod
+@Mod(TutorialMod1.MOD_ID)
+public class TutorialMod1
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    public static final String MOD_ID="tutorialmod";
+    public static final String MOD_ID="tutorialmod1";
 
-    public TutorialMod()
+    public TutorialMod1()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
